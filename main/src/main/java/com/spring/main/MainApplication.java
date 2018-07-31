a9b6bd21bfee62c5cf08572677ec47cedce50ea5
EricMint <shaozhiqi@gmail.com>
2018-08-01 01:01:43 +0800

    configure custom DataSource

--- main/src/main/java/com/spring/main/MainApplication.java
+++ main/src/main/java/com/spring/main/MainApplication.java
@@ -3,15 +3,26 @@ package com.spring.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com")
-@EnableJpaRepositories(basePackages = {"com"})
-@EntityScan(basePackages = {"com"})
+import javax.sql.DataSource;
+
+@SpringBootApplication(scanBasePackages = {"com.spring.main", "com.spring.data"})
+//@EnableJpaRepositories(basePackages = {"com.spring.data.repository"})
+@EntityScan(basePackages = {"com.spring.data.entity"})
 public class MainApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
     }
 
+    @Bean
+    @ConfigurationProperties(prefix = "app.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
 }
+
