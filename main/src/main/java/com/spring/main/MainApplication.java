84c27a2d0bb29a208269f7b95fc9c5d03a3d1694
EricMint <shaozhiqi@gmail.com>
2018-08-19 22:28:27 +0800

    use default database settings

--- main/src/main/java/com/spring/main/MainApplication.java
+++ main/src/main/java/com/spring/main/MainApplication.java
@@ -3,15 +3,8 @@ package com.spring.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication(scanBasePackages = {"com.spring.main", "com.spring.data"})
-//@EnableJpaRepositories(basePackages = {"com.spring.data.repository"})
 @EntityScan(basePackages = {"com.spring.data.entity"})
 public class MainApplication {
 
@@ -19,10 +12,5 @@ public class MainApplication {
         SpringApplication.run(MainApplication.class, args);
     }
 
-    @Bean
-    @ConfigurationProperties(prefix = "app.datasource")
-    public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
-    }
 }
 
